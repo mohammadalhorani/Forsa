d8e2987f80748f6dcc37b44099ecbec25dbb0831
mohammadalhorani <46869212+mohammadalhorani@users.noreply.github.com>
2024-03-10 12:56:34 +0300

    removing unnecessary details

--- app/Http/Livewire/BellIcon.php
+++ app/Http/Livewire/BellIcon.php
@@ -58,33 +58,33 @@ class BellIcon extends Component
 
     public function mount()
     {
-        // Fetch the last 10 notes when the component is mounted
+      
         $this->lastNotes = $this->getLastNotes(10);
 
-        // Calculate the unread count
+
         $this->unreadCount = $this->calculateUnreadCount();
 
-        // Initialize showContent
+
         $this->showContent = $this->unreadCount > 0;
     }
 
     public function markAsRead($noteId)
     {
-        // Logic to mark the note as read (update your database or any other action)
+
 
 
           $readNotes=Notes::find($noteId);
           $readNotes->read=true;
           $readNotes->save();
-        // Remove the clicked note from lastNotes
+
         $this->lastNotes = $this->lastNotes->filter(function ($note) use ($noteId) {
             return $note->id !== $noteId;
         });
 
-        // Recalculate the unread count after marking a note as read
+
         $this->unreadCount = $this->calculateUnreadCount();
 
-        // Update showContent based on the unread count
+
         $this->showContent = $this->unreadCount > 0;
     }
 
@@ -95,13 +95,13 @@ class BellIcon extends Component
 
     protected function calculateUnreadCount()
     {
-        // Logic to calculate the unread count based on your application's requirements
+
         return Notes::where('read', false)->count();
     }
 
     public function showLastNewNotes()
     {
-        // Toggle the state
+
         $this->showContent = !$this->showContent;
     }
 
